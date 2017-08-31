06e19fd0943e2b5bae7c6aa37b350aaee5e47850
gvazqu <gvazqu@outlook.com>
2017-08-31 15:11:49 -0400

    Create init.js
    initial material components etc.

---
+++ init.js
@@ -0,0 +1,23 @@
+(function($){
+
+  $(function(){
+        //initialize all modals          
+       	 $('.modal').modal({
+      	dismissible: true, // Modal can be dismissed by clicking outside of the modal
+      	opacity: .5, // Opacity of modal background
+      	inDuration: 400, // Transition in duration
+      	outDuration: 400, // Transition out duration
+      	startingTop: '4%', // Starting top style attribute
+     	endingTop: '10%', // Ending top style attribute
+      ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available. 
+        console.log(modal, trigger);
+      },      
+    }
+  );	
+	
+
+    	$('.carousel.carousel-slider').carousel();
+	$('.carousel.carousel-slider').carousel({fullWidth: true});
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
